b162a7ed216f0cf1398f80f7c287673b51d2c9f2
Ayush <aks010598@gmail.com>
2019-03-16 12:07:35 +0530

    Added frontend and connected form to backend

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import List
+from .forms import ListForm
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
-    return render(request,'todo_list/home.html',{})
+    if request.method=='POST':
+        form=ListForm(request.POST or None)
+        if(form.is_valid):
+            form.save()
+            all_items=List.objects.all
+            messages.success(request,('Item added!'))
+            return render(request,'todo_list/home.html',{'all_items':all_items})
+
+    else:
+        all_items=List.objects.all
+        return render(request,'todo_list/home.html',{'all_items':all_items})
+
